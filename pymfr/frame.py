1a782bfd193537cf70ed3ca24dd4d73374fd78cc
MicleBrick <6323125+MicleBrick@users.noreply.github.com>
2022-08-15 11:50:48 -0400

    add public api for calculating vht

--- pymfr/frame.py
+++ pymfr/frame.py
@@ -1,48 +1,115 @@
 import torch
 
 
+def estimate_ht_frame(magnetic_field: torch.tensor,
+                      electric_field: torch.tensor):
+    """
+    Estimate the DeHoffman-Teller (HT) frame based on magnetic field and electric field data such that
+    the electric field in such a frame is zero.
+
+    References:
+
+    :param magnetic_field: Tensor with shape ([B, ]N, 3).
+    :param electric_field: Tensor with shape ([B, ]N, 3).
+    If electric field measurements are unavailable, it can be estimates at -(v cross B).
+    If available, electron velocity should be used to estimate the electric field.
+    Otherwise, proton velocity can be used.
+    :return Tensor with shape (B, 3)
+    """
+
+    assert magnetic_field.shape == electric_field.shape
+    assert len(magnetic_field.shape) >= 2
+    assert magnetic_field.shape[-1] == 3
+    assert magnetic_field.device == electric_field.device
+
+    Bx, By, Bz = magnetic_field[..., 0], magnetic_field[..., 1], magnetic_field[..., 2]
+
+    coefficients = [[By ** 2 + Bz ** 2, -Bx * By, -Bx * Bz],
+                    [-Bx * By, Bx ** 2 + Bz ** 2, -By * Bz],
+                    [-Bx * Bz, -By * Bz, Bx ** 2 + By ** 2]]
+    coefficient_matrix = torch.zeros((*magnetic_field.shape[:-2], 3, 3), device=magnetic_field.device)
+    for i in range(3):
+        for j in range(3):
+            coefficient_matrix[..., i, j] = coefficients[i][j].mean(dim=-1)
+
+    dependent_values = torch.cross(electric_field, magnetic_field, dim=-1).mean(dim=-2)
+
+    fitting_result = torch.linalg.lstsq(coefficient_matrix, dependent_values)
+    return fitting_result.solution
+
+
+def estimate_ht2d_frame(magnetic_field: torch.tensor,
+                        electric_field: torch.tensor,
+                        axes: torch.tensor):
+    """
+    Find the frame where the electric field along the axis is zero.
+
+    References:
+
+    :param magnetic_field: Tensor with shape ([B, ]N, 3)
+    :param electric_field: Tensor with shape ([B, ]N, 3)
+    :param axes: Tensor with shape ([B, [M, ]], 3)
+    The covariance matrices will be calculated once for
+    each batch and then the frame will be calculated for each axis
+    if multiple axes are provided.
+    :return: Tensor with shape ([B, [M, ]], 3)
+    """
+
+    assert magnetic_field.shape == electric_field.shape
+    assert len(magnetic_field.shape) >= 2
+    assert magnetic_field.shape[-1] == axes.shape[-1] == 3
+    assert magnetic_field.device == electric_field.device == axes.device
+    multiple_axes = len(axes.shape) > 2
+    if multiple_axes:
+        assert axes.shape[:-2] == magnetic_field.shape[:-2]
+    else:
+        assert axes.shape[:-1] == magnetic_field.shape[:-2]
+
+    MBB = _covariance(magnetic_field, magnetic_field)
+    MBE = _covariance(magnetic_field, electric_field)
+    solutions = torch.linalg.solve(MBB, MBE)
+
+    if multiple_axes:
+        # add extra dimension after batch dimensions for axes
+        solutions = solutions.unsqueeze(-3).expand(*((-1,) * (len(axes.shape) - 2)), axes.shape[-2], -1, -1)
+
+    frames = torch.cross(axes, (solutions @ axes.unsqueeze(-1)).squeeze(-1))
+    return frames
+
+
+def _covariance(a, b):
+    assert a.shape == b.shape
+    assert len(a.shape) >= 2
+
+    d_a = a - a.mean(dim=-2, keepdim=True)
+    d_b = b - b.mean(dim=-2, keepdim=True)
+    matrix = torch.zeros((*a.shape[:-2], a.shape[-1], a.shape[-1]), device=a.device)
+    for i in range(3):
+        for j in range(3):
+            matrix[..., i, j] = (d_a[..., i] * d_b[..., j]).mean(dim=-1)
+    return matrix
+
+
 def _find_frames(magnetic_field, velocity, trial_axes, frame_type):
-    batch_axes = torch.repeat_interleave(trial_axes, repeats=len(magnetic_field), dim=0)
+    n_batch = len(magnetic_field)
+    batch_axes = torch.repeat_interleave(trial_axes, repeats=n_batch, dim=0)
 
     electric_field = -torch.cross(velocity, magnetic_field, dim=2)
 
     if frame_type == "mean_velocity":
         batch_frames = velocity.mean(dim=1).repeat(len(trial_axes), 1)
     elif frame_type == "vht_2d":
-        MBB = _covariance(magnetic_field, magnetic_field)
-        MBE = _covariance(magnetic_field, electric_field)
-        solutions = torch.linalg.solve(MBB, MBE)
+        axes = trial_axes.unsqueeze(0).expand(n_batch, -1, -1)
+        batch_frames = estimate_ht2d_frame(magnetic_field, electric_field, axes)
 
-        solutions = solutions.repeat(len(trial_axes), 1, 1)
-        batch_frames = torch.cross(batch_axes, (solutions @ batch_axes.unsqueeze(2)).squeeze(2))
-
+        # output will be (batch, axis, 3), but we made each axis repeated "batch" times,
+        # so the same axis should be repeated and the batch should change every entry,
+        # so before flattening, need to transpose
+        batch_frames = batch_frames.transpose(0, 1).reshape(-1, 3)
     elif frame_type == "vht":
-        Bx = magnetic_field[:, :, 0]
-        By = magnetic_field[:, :, 1]
-        Bz = magnetic_field[:, :, 2]
-
-        coefficients = [[(By ** 2 + Bz ** 2).mean(dim=1), (-Bx * By).mean(dim=1), (-Bx * Bz).mean(dim=1)],
-                        [(-Bx * By).mean(dim=1), (Bx ** 2 + Bz ** 2).mean(dim=1), (-By * Bz).mean(dim=1)],
-                        [(-Bx * Bz).mean(dim=1), (-By * Bz).mean(dim=1), (Bx ** 2 + By ** 2).mean(dim=1)]]
-        coefficient_matrix = torch.stack(
-            [torch.stack(x, dim=1) for x in coefficients], dim=2)
-
-        dependent_values = torch.cross(electric_field, magnetic_field, dim=2).mean(dim=1)
-
-        fitting_result = torch.linalg.lstsq(coefficient_matrix, dependent_values)
-        vht = fitting_result.solution
+        vht = estimate_ht_frame(magnetic_field, electric_field)
         batch_frames = vht.repeat(len(trial_axes), 1)
     else:
         raise Exception(f"Unknown frame type {frame_type}")
 
     return batch_frames, batch_axes
-
-
-def _covariance(x, y):
-    dx = x - x.mean(dim=1, keepdim=True)
-    dy = y - y.mean(dim=1, keepdim=True)
-    coefficients = [[(dx[:, :, i] * dy[:, :, j]).mean(dim=1) for j in range(3)]
-                    for i in range(3)]
-    rows = [torch.stack(columns, dim=1) for columns in coefficients]
-    stack = torch.stack(rows, dim=1)
-    return stack
