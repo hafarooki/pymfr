13bdc312edfd36cf6a59a67e24b677392173e090
MicleBrick <6323125+MicleBrick@users.noreply.github.com>
2022-08-14 22:03:58 -0400

    update setup.py

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+try:
+    import torch
+except ImportError:
+    raise Exception("pymfr requires PyTorch")
+
+setup(
+    name="pymfr",
+    version="1.0.0",
+    author="Hameedullah A. Farooki",
+    author_email="haf5@njit.edu",
+    packages=["pymfr"],
+    keywords="pymfr fluxrope mfr smfr grad-shafranov gs",
+    install_requires=["torch>=1.8.2"]
+)
