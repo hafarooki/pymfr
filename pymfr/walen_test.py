efeb7d3562a5766c827742d5fc08add1fa771c1a
MicleBrick <6323125+MicleBrick@users.noreply.github.com>
2022-08-16 00:44:48 -0400

    fix walen test

--- pymfr/walen_test.py
+++ pymfr/walen_test.py
@@ -2,9 +2,9 @@ import torch
 
 
 def _calculate_alfvenicity(frame, windows):
-    remaining_flow = (windows[:, :, 3:6] - frame.unsqueeze(1)).squeeze(1)
-    alfven_velocity = windows[:, :, 6:9]
-    d_flow = remaining_flow - remaining_flow.mean(dim=1).unsqueeze(1)
-    d_alfven = alfven_velocity - alfven_velocity.mean(dim=1).unsqueeze(1)
-    walen_slope = (d_flow * d_alfven).sum(dim=(1, 2)) / (d_alfven ** 2).sum(dim=(1, 2))
+    remaining_flow = (windows[:, :, 3:6] - frame.unsqueeze(1)).squeeze(1).flatten(1)
+    alfven_velocity = windows[:, :, 6:9].flatten(1)
+    d_flow = remaining_flow - remaining_flow.mean(dim=1, keepdim=True)
+    d_alfven = alfven_velocity - alfven_velocity.mean(dim=1, keepdim=True)
+    walen_slope = (d_flow * d_alfven).sum(dim=1) / (d_alfven ** 2).sum(dim=1)
     return torch.abs(walen_slope)
