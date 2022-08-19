37fe570769eb68b8f6abbecd5ac5cb5995aaf07b
MicleBrick <6323125+MicleBrick@users.noreply.github.com>
2022-08-19 00:21:33 -0400

    fix calculations

--- pymfr/axis.py
+++ pymfr/axis.py
@@ -1,4 +1,5 @@
 import numpy as np
+import scipy.constants
 import torch
 import torch.nn.functional as F
 
@@ -17,25 +18,38 @@ def minimize_rdiff(magnetic_field, gas_pressure, frame_velocity, iterations=3):
     best_axis = None
     best_rdiff = None
 
-    batch_axes = _get_trial_axes(range(0, 90, 5), range(0, 360, 10)).to(magnetic_field.device)
+    batch_axes = _get_trial_axes(range(0, 90, 1), range(0, 360, 2)).to(magnetic_field.device)
     for i in range(iterations):
         batch_field = magnetic_field.unsqueeze(0).expand(len(batch_axes), -1, -1)
         batch_frames = frame_velocity.unsqueeze(0).expand(len(batch_axes), -1)
         batch_gas_pressure = gas_pressure.unsqueeze(0).expand(len(batch_axes), -1)
 
         rotated_field = _rotate_field(batch_axes, batch_field, batch_frames)
-        transverse_pressure = batch_gas_pressure + rotated_field[:, :, 2] * 2
+        transverse_pressure = batch_gas_pressure + (rotated_field[:, :, 2] * 1e-9) ** 2 / (2 * scipy.constants.mu_0) * 1e9
         potential = torch.zeros((len(rotated_field)), rotated_field.shape[1], device=rotated_field.device)
         potential[:, 1:] = torch.cumulative_trapezoid(rotated_field[:, :, 1])
 
         inflection_points = potential.abs().argmax(dim=1)
 
+        peaks = transverse_pressure[torch.arange(transverse_pressure.shape[0], device=transverse_pressure.device),
+                                    inflection_points]
+        min_pressure, max_pressure = torch.aminmax(transverse_pressure, dim=1)
+        thresholds = torch.quantile(transverse_pressure, 0.85, dim=1, interpolation="lower")
+
+        mask = (inflection_points > 0) & \
+               (inflection_points < (potential.shape[-1] - 1)) & \
+               (peaks > thresholds) & \
+               ((potential.abs()[:, -1] / potential.abs().amax(dim=1)) < 0.1) & \
+               ((max_pressure - min_pressure) > 0)
+
         rdiff = _calculate_residue_diff(inflection_points, potential, transverse_pressure)
+        rdiff = torch.where(mask, rdiff, torch.inf)
 
         argmin = torch.argmin(rdiff)
         if best_rdiff is None or rdiff[argmin] < best_rdiff:
-            best_axis = batch_axes[argmin]
-            best_rdiff = rdiff[argmin]
+            if rdiff[argmin] < torch.inf:
+                best_axis = batch_axes[argmin]
+                best_rdiff = rdiff[argmin]
 
         if best_axis is None:
             break
