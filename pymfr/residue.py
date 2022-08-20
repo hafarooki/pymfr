d385a056a10c9742a091067e6b9880c837b86e5f
MicleBrick <6323125+MicleBrick@users.noreply.github.com>
2022-08-20 19:02:06 -0400

    require at least 1/4 duration on each branch after trimming

--- pymfr/residue.py
+++ pymfr/residue.py
@@ -26,19 +26,19 @@ def _calculate_residue_diff(inflection_points, potential, pressure):
     duration = folded_data.shape[2]
 
     all_indices = torch.arange(duration, device=inflection_points.device)
-    all_indices = all_indices.unsqueeze(0).unsqueeze(1).expand(folded_data.shape[0], 2, -1)
-    before = all_indices < inflection_points.unsqueeze(1).unsqueeze(2)
-    after = all_indices > inflection_points.unsqueeze(1).unsqueeze(2)
+    all_indices = all_indices.unsqueeze(0).expand(folded_data.shape[0], -1)
+    before = all_indices < inflection_points.unsqueeze(1)
+    after = all_indices > inflection_points.unsqueeze(1)
 
     indices = torch.arange(len(folded_data), device=folded_data.device)
     inflection_point_values = folded_data[indices, :2, inflection_points]
     inflection_point_values = inflection_point_values.unsqueeze(2).expand(-1, -1, duration)
 
     # in first half, values after inflection point should be the inflection points repeated
-    folded_data[:, :2, :] = torch.where(after, inflection_point_values, folded_data[:, :2, :])
+    folded_data[:, :2, :] = torch.where(after.unsqueeze(1), inflection_point_values, folded_data[:, :2, :])
 
     # in second half, values before inflection point should be the inflection points repeated
-    folded_data[:, 2:, :] = torch.where(before, inflection_point_values, folded_data[:, 2:, :])
+    folded_data[:, 2:, :] = torch.where(before.unsqueeze(1), inflection_point_values, folded_data[:, 2:, :])
 
     sort1 = torch.argsort(folded_data[:, 0, :], dim=1)
     folded_data[:, 0, :] = folded_data[:, 0, :].gather(1, sort1)
@@ -71,8 +71,9 @@ def _calculate_residue_diff(inflection_points, potential, pressure):
     # infinity if 0 pressure range
     error_diff = torch.where(pressure_range > 0, error_diff, torch.inf)
 
-    # require at least half of duration after trimming
-    error_diff = torch.where((potential >= minimum_values).long().sum(dim=1) >= duration // 2,
+    # require at least 1/4 duration on each branch after trimming
+    error_diff = torch.where((((potential >= minimum_values) & before).long().sum(dim=1) >= duration // 4)
+                             & (((potential >= minimum_values) & after).long().sum(dim=1) >= duration // 4),
                              error_diff, torch.inf)
 
     return error_diff
